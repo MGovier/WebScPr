e68bc0c67183153985bfdc730720a4cc7a4df075
Merlin Govier <merlingovier@gmail.com>
2013-12-10 00:08:04 +0000

    Added footer

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+	</div>
+
+	<div class="footer">
+
+		<div class="container">
+
+			<ul>		
+				<li>Created by UP663652 at the University of Portsmouth</li>
+			</ul>
+
+			<p>&copy;<?php echo date('Y'); STORE_NAME?></p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
