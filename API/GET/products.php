4b8802846a99cea69ab9bd5b81ef179d2ec7683f
Merlin Govier <merlingovier@gmail.com>
2014-01-15 20:11:52 +0000

    Added dodgy responsive categories.

--- API/GET/products.php
+++ API/GET/products.php
@@ -1,13 +1,13 @@
-	    	<?php
-				require_once($_SERVER["DOCUMENT_ROOT"] . "/663652/OnShop/inc/config.php");
-	    		include(ROOT_PATH . "inc/db/database.php");
-	    		include(ROOT_PATH . "inc/db/insert-demo-products.php");
-	    		insert_demo_data($db);
-	    		$products = $db->query("SELECT * FROM PRODUCTS");
-	    		$productsArray = array();
-	    		while ($row = $products->fetch_assoc()) {
-	    			$row["PRODUCT_IMAGE"] = BASE_URL . $row["PRODUCT_IMAGE"];
-	    			$productsArray[] = $row;
-	    		}
-	    		echo json_encode($productsArray);
-	    	?>
+<?php
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/663652/OnShop/inc/config.php");
+	include(ROOT_PATH . "inc/db/database.php");
+	include(ROOT_PATH . "inc/db/insert-demo-products.php");
+	insert_demo_data($db);
+	$products = $db->query("SELECT * FROM PRODUCTS");
+	$productsArray = array();
+	while ($row = $products->fetch_assoc()) {
+		$row["PRODUCT_IMAGE"] = BASE_URL . $row["PRODUCT_IMAGE"];
+		$productsArray[] = $row;
+	}
+	echo json_encode($productsArray);
+?>
