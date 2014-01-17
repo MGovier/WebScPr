a3e81bd45b8b49a8fad09a50ae86b98d70cb14fc
Merlin Govier <merlingovier@gmail.com>
2014-01-17 18:11:54 +0000

    Added product URLs, override usual <a> click function using return false

--- API/GET/products.php
+++ API/GET/products.php
@@ -7,6 +7,7 @@
 	$productsArray = array();
 	while ($row = $products->fetch_assoc()) {
 		$row["PRODUCT_IMAGE"] = BASE_URL . $row["PRODUCT_IMAGE"];
+		$row["PRODUCT_URL"] = BASE_URL . "product.php?id=" . $row["PRODUCT_ID"];
 		$productsArray[] = $row;
 	}
 	echo json_encode($productsArray);
--- js/OnShopScripts.js
+++ js/OnShopScripts.js
@@ -1,37 +1,41 @@
-	var xhr, target, changeListener;
+pageLoaded = function () {
+	getAllProducts();
+};
 
-	pageLoaded = function () {
-		getAllProducts();
-	};
-
-	getAllProducts = function () {
-		target = document.getElementById("products");
-		target.innerHTML = ('<img class="loader" src="img/ajax-loader.gif" alt="Loading">')
-		xhr = new XMLHttpRequest();
-		changeListener = function () {
-			if (xhr.readyState === 4 && xhr.status === 200) {
-				var productsArray = eval(xhr.responseText);
-				target.innerHTML = styleProducts(productsArray);
-			} else {
-				target.innerHTML = "<p>Something went wrong.</p>";
-			}
+getAllProducts = function () {
+	var target = document.getElementById("products");
+	target.innerHTML = ('<img class="loader" src="img/ajax-loader.gif" alt="Loading">')
+	var xhr = new XMLHttpRequest();
+	changeListener = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var productsArray = eval(xhr.responseText);
+			target.innerHTML = styleProducts(productsArray);
+		} else {
+			target.innerHTML = "<p>Something went wrong.</p>";
 		}
-		xhr.open("GET", "API/GET/products.php", true);
-		xhr.onreadystatechange = changeListener;
-		xhr.send();
-	};
+	}
+	xhr.open("GET", "API/GET/products.php", true);
+	xhr.onreadystatechange = changeListener;
+	xhr.send();
+};
 
-	styleProducts = function (productsArray) {
-		var formattedProducts = "";
-		for (var i = productsArray.length - 1; i >= 0; i--) {
-		 	var product = productsArray[i];
-		 	formattedProducts += '<li><a href="#"><h4 class="productName">' 
-		 					+ product["PRODUCT_NAME"] + '</h4>'
-		 					+ '<img src="' + product["PRODUCT_IMAGE"]
-		 					+ '" alt="' + product["PRODUCT_NAME"] + '"><p class="description">'
-		 					+ product["PRODUCT_DESCRIPTION"].substring(0,110) + '...</p>'
-		 					+ '<p class="cost">&pound;' + product["PRODUCT_PRICE"].toString() + '</p></li>';
-		 	}
-		return formattedProducts;
-	};
+styleProducts = function (productsArray) {
+	var formattedProducts = "";
+	for (var i = productsArray.length - 1; i >= 0; i--) {
+	 	var product = productsArray[i];
+	 	formattedProducts += '<li><a href="' + product["PRODUCT_URL"] + '"' + 'onclick="return getProduct(' + product["PRODUCT_ID"] + ');"' 
+	 					+ '><h4 class="productName">' 
+	 					+ product["PRODUCT_NAME"] + '</h4>'
+	 					+ '<img src="' + product["PRODUCT_IMAGE"]
+	 					+ '" alt="' + product["PRODUCT_NAME"] + '"><p class="description">'
+	 					+ product["PRODUCT_DESCRIPTION"].substring(0,110) + '...</p>'
+	 					+ '<p class="cost">&pound;' + product["PRODUCT_PRICE"].toString() + '</a></p></li>';
+	 	}
+	return formattedProducts;
+};
+
+getProduct = function(productID) {
+	return false;
+};
+
 window.addEventListener("load", pageLoaded);
