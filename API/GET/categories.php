7f0a8887003e5d88dbbb75354409e59cdc0e89ea
Merlin Govier <merlingovier@gmail.com>
2014-02-04 17:57:59 +0000

    Fixed double declaration

--- API/GET/categories.php
+++ API/GET/categories.php
@@ -4,7 +4,7 @@
 	if (isset($_GET["empty"]) && $_GET["empty"] === "false") {
 		$categories = $db->query("SELECT * FROM CATEGORIES AS C, PRODUCTS AS P WHERE C.CATEGORY_ID = P.CATEGORY_ID GROUP BY C.CATEGORY_ID");
 	} elseif (isset($_GET["products"]) && $_GET["products"] === "count") {
-		$categories = $categories = $db->query("SELECT C.CATEGORY_ID, C.CATEGORY_NAME, COUNT(P.PRODUCT_ID) AS COUNT FROM CATEGORIES C LEFT JOIN PRODUCTS P ON C.CATEGORY_ID = P.CATEGORY_ID GROUP BY C.CATEGORY_ID");
+		$categories = $db->query("SELECT C.CATEGORY_ID, C.CATEGORY_NAME, COUNT(P.PRODUCT_ID) AS COUNT FROM CATEGORIES C LEFT JOIN PRODUCTS P ON C.CATEGORY_ID = P.CATEGORY_ID GROUP BY C.CATEGORY_ID");
 	}
 	else {
 		$categories = $db->query("SELECT * FROM CATEGORIES");
